0290644fd4c7767529585971aec0effc10b8ee0b
Shahrad Laptop <shahrad.shj@gmail.com>
2024-06-03 13:56:23 -0700

    chore: Add configuration files for training Harry Potter character-level model

--- config/train_harry_potter_char.py
+++ config/train_harry_potter_char.py
@@ -1,7 +1,7 @@
-# train a miniature character-level shakespeare model
+# train a miniature character-level harry potter model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-shakespeare-char'
+out_dir = 'out-harry-potter-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -10,10 +10,10 @@ log_interval = 10 # don't print too too often
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'shakespeare-char'
+wandb_project = 'harry-potter-char'
 wandb_run_name = 'mini-gpt'
 
-dataset = 'shakespeare_char'
+dataset = 'harry_potter_char'
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
---
+++ data/harry_potter_char/readme.md
@@ -0,0 +1,9 @@
+
+# Harry Potter, character-level
+
+Harry Potter, of the good old char-rnn fame :) Treated on character-level.
+
+After running `prepare.py`:
+
+- train.bin has 1,003,854 tokens
+- val.bin has 111,540 tokens
